95fda92ffa930bb54cb60b44361f8f4af6bcaca5
LonelyMelancholy <106623980+LonelyMelancholy@users.noreply.github.com>
2025-12-12 11:51:54 +0300

    Modify install.sh to fetch latest node_exporter version
    Updated the install script to download the latest version of node_exporter dynamically.

--- install.sh
+++ install.sh
@@ -5,8 +5,25 @@ sudo useradd --no-create-home --shell /usr/sbin/nologin node_exporter
 
 useradd -r -s /bin/false node_exporter
 # Скачиваем, распаковываем
-wget 
+LATEST_TAG=$(
+  curl -Ls -o /dev/null -w '%{url_effective}' \
+    "https://github.com/prometheus/node_exporter/releases/latest" \
+  | awk -F'/tag/' '{print $2}'
+)
+
+echo "$LATEST_TAG"  # v1.8.2
+
+OS_ARCH="linux-amd64"
+FILENAME="node_exporter-${LATEST_TAG#v}.${OS_ARCH}.tar.gz"
+
+URL="https://github.com/prometheus/node_exporter/releases/download/$LATEST_TAG/$FILENAME"
+
+curl -L -o "$FILENAME" "$URL"
+
+
 tar -xvf node_exporter-1.9.1.linux-amd64.tar.gz
+
+
 # Копируем в директорию к бинарникам
 sudo cp node_exporter-*/node_exporter /usr/local/bin/
 
